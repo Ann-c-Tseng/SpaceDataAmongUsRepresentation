7af8b1524d4942e441bc3ae99197c86a67805147
Ann-c-Tseng <ann.c.tseng@gmail.com>
2021-11-02 22:21:24 -0700

    test

---
+++ sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background('#000000');
+  noStroke();
+  loadJSON("http://api.open-notify.org/astros.json", dataReceived);
+
+  img = createImg('./amongus.png');
+  img.hide();
+}
+
+function dataReceived(data){
+  for (let i = 0; i < data.number; i++){
+      var wth = random(200, width-200);
+      var hgt = random(200, height-200);
+
+      fill('#ffffff');
+      text(data.people[i]['name'], wth, hgt);
+      image(img,wth,hgt,50,40);
+      text(data.people[i]['craft'], wth, hgt+50);
+  }
+}
+
+function draw() { 
+}
